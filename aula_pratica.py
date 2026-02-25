42d3926b3237062f03910796c953b32e1994289a
Alexandro Allan Alves Santos Eichstaedt <alexandroartanime@gmail.com>
2026-02-25 19:07:40 -0300

    atualizacao str

---
+++ aula_pratica.py
@@ -0,0 +1,35 @@
+#mkdir nome da pasta
+#cd nome da pasta para entrar
+#git init para iniciar no vscode
+#git config --global user.name "Seu Nome Completo"
+#git config --global user.email "seuemail@gmail.com"
+#adicionar novo arquivo na nuvem git add remote add link
+#git remote add origin https://github.com/Alexandro-Eichstaedt/pratica_datascience.git
+#git commit -m "primeiro commit"
+
+'''
+Criar repositório no GitHub
+
+Entre no GitHub
+
+Clique em New repository
+
+NÃO marque "Add README"
+
+Copie a URL do repositório
+
+Exemplo:
+
+https://github.com/Alexandro-Eichstaedt/aulas_datascience.git
+'''
+
+#conectar com o github
+#git remote add origin https://github.com/Alexandro-Eichstaedt/aulas_datascience.git
+
+#criar branch
+#Definir branch main
+#git branch -M main
+#enviar para o git
+#git push -u origin main
+#alternar para master
+#git switch master
